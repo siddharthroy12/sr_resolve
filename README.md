4c6de5023a20524aef4430d1a2b41ca42f01cda8
Siddharth Roy <siddharthroy36912@gmail.com>
2021-09-30 07:33:35 +0530

    New raycast method which resolves collisions accurately

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ A simple single header C/C++ Library for AABB Collision detection and resolution
 ## Structs
 
 ```c
+// Rectangle
 typedef struct sr_rec {
 	float x;
 	float y;
@@ -14,18 +15,26 @@ typedef struct sr_rec {
 	float height;
 } sr_rec;
 
+// 2D Vector
 typedef struct sr_vec2 {
 	float x;
 	float y;
+} sr_vec2;
+
+// 2D Ray
+typedef struct sr_vec2 {
+  sr_vec2 position;
+  sr_vec2 direction;
 } sr_vec2;
 ```
 
 ## Function
 ```c
-sr_check_collision_recs(sr_rec rec1, sr_rec rec2);
-sr_get_collision_rec(sr_rec rec1, sr_rec rec2);
-sr_rec sr_resolver_rects_collision(sr_rec static_rec, sr_rec rec_to_move);
-sr_rec sr_move_and_collide(sr_rec *static_recs, int number_of_recs, sr_rec rec_to_move, sr_vec2 velocity)
+bool sr_check_rec_vs_rec_collision(sr_rec rec1, sr_rec rec2);
+bool sr_check_ray_vs_rec_collision(const sr_ray2 ray, const sr_rec target, sr_vec2 *contact_point, sr_vec2 *contact_normal, float *t_hit_near);
+bool sr_dynamic_rect_vs_rect(const sr_rec in, const sr_rec target, sr_vec2 vel, sr_vec2 *contact_point, sr_vec2 *contact_normal, float *contact_time, float delta)
+// The function you will use in your game
+void sr_move_and_slide(sr_rec *obstacles, int obstacles_length, sr_vec2 hitbox, sr_vec2 *vel, sr_vec2 *pos , float delta);
 ```
 
 ## Example
--- sr_resolve.h
+++ sr_resolve.h
@@ -4,12 +4,16 @@
 #ifndef SR_RESOLVE_H
 #define SR_RESOLVE_H
 #include <math.h>
+#include <stdio.h>
+
+#define max(x, y) (x > y ? x : y)
+#define min(x, y) (x > y ? y : x)
 
 typedef struct sr_rec {
 	float x;
-    float y;
-    float width;
-    float height;
+  	float y;
+  	float width;
+  	float height;
 } sr_rec;
 
 typedef struct sr_vec2 {
@@ -17,159 +21,217 @@ typedef struct sr_vec2 {
 	float y;
 } sr_vec2;
 
-// Get the length of sr_vec2
+typedef struct sr_ray2 {
+    sr_vec2 position;
+    sr_vec2 direction;
+} sr_ray2;
+
+typedef struct sr_sort_pair {
+    int index;
+    float time;
+} sr_sort_pair;
+
+void swap_float(float *vec1, float *vec2) {
+  	float temp = *vec1;
+  	*vec1 = *vec2;
+  	*vec2 = temp;
+}
+
+// Get the length of a vector
 float sr_vec2_length(sr_vec2 v) {
 	float result = sqrtf((v.x*v.x) + (v.y*v.y));
-    return result;
+  	return result;
 }
 
-// Scale sr_vec2
+// Scale a vector by a given float value
 static sr_vec2 sr_vec2_scale(sr_vec2 v, float scale) {
-    sr_vec2 result = { v.x*scale, v.y*scale };
-    return result;
+  	return (sr_vec2){ v.x*scale, v.y*scale };
 }
 
-// Normalize sr_vec2
+// Divide two vectors
+static sr_vec2 sr_vec2_divide(sr_vec2 v1, sr_vec2 v2) {
+  	return (sr_vec2){ v1.x/v2.x, v1.y/v2.y };
+}
+
+// Multiply two vectors
+static sr_vec2 sr_vec2_multiply(sr_vec2 v1, sr_vec2 v2) {
+  	return (sr_vec2){ v1.x*v2.x, v1.y*v2.y };
+}
+
+// Normalize a vector
 static sr_vec2 sr_vec2_normalize(sr_vec2 v) {
-    sr_vec2 result = sr_vec2_scale(v, 1 / sr_vec2_length(v));
-    return result;
+  	return sr_vec2_scale(v, 1 / sr_vec2_length(v));
 }
 
-// Substract sr_vec2
+// Substract two vectors
 static sr_vec2 sr_vector2_sub(sr_vec2 v1, sr_vec2 v2) {
-    sr_vec2 result = { v1.x - v2.x, v1.y - v2.y };
-    return result;
+  	return (sr_vec2){ v1.x - v2.x, v1.y - v2.y };
 }
 
-// Check collision between two sr_rec
-static bool sr_check_collision_recs(sr_rec rec1, sr_rec rec2) {
-    bool collision = false;
+// Add two vectors
+static sr_vec2 sr_vector2_add(sr_vec2 v1, sr_vec2 v2) {
+  	return (sr_vec2){ v1.x + v2.x, v1.y + v2.y };
+}
 
+// Check collision between two rectangles
+static bool sr_check_rec_vs_rec_collision(sr_rec rec1, sr_rec rec2) {
     if (
 		(rec1.x < (rec2.x + rec2.width) && (rec1.x + rec1.width) > rec2.x) &&
         (rec1.y < (rec2.y + rec2.height) && (rec1.y + rec1.height) > rec2.y)
 	) {
-		collision = true;
-	} 
-
-    return collision;
-}
-
-// Get the area of the collision
-static sr_rec sr_get_collision_rec(sr_rec rec1, sr_rec rec2) {
-    sr_rec rec = { 0, 0, 0, 0 };
-
-    if (sr_check_collision_recs(rec1, rec2)) {
-        float dxx = fabs(rec1.x - rec2.x);
-        float dyy = fabs(rec1.y - rec2.y);
-
-        if (rec1.x <= rec2.x) {
-            if (rec1.y <= rec2.y) {
-                rec.x = rec2.x;
-                rec.y = rec2.y;
-                rec.width = rec1.width - dxx;
-                rec.height = rec1.height - dyy;
-            } else {
-                rec.x = rec2.x;
-                rec.y = rec1.y;
-                rec.width = rec1.width - dxx;
-                rec.height = rec2.height - dyy;
-            }
-        } else {
-            if (rec1.y <= rec2.y) {
-                rec.x = rec1.x;
-                rec.y = rec2.y;
-                rec.width = rec2.width - dxx;
-                rec.height = rec1.height - dyy;
-            } else {
-                rec.x = rec1.x;
-                rec.y = rec1.y;
-                rec.width = rec2.width - dxx;
-                rec.height = rec2.height - dyy;
-            }
-        }
-
-        if (rec1.width > rec2.width) {
-            if (rec.width >= rec2.width) rec.width = rec2.width;
-        }
-        else {
-            if (rec.width >= rec1.width) rec.width = rec1.width;
-        }
-
-        if (rec1.height > rec2.height) {
-            if (rec.height >= rec2.height) rec.height = rec2.height;
-        } else {
-           if (rec.height >= rec1.height) rec.height = rec1.height;
-        }
-    }
-
-    return rec;
-}
-
-// Get the vector between the center of two rects
-static sr_vec2 sr_get_center_distance_vec(sr_rec rec1, sr_rec rec2) {
-    sr_vec2 rec1_center = (sr_vec2){
-        .x = rec1.x + (rec1.width/2),
-        .y = rec1.y + (rec1.height/2)
-    };
-
-    sr_vec2 rec2_center = (sr_vec2) {
-        .x = rec2.x + (rec2.width/2),
-        .y = rec2.y + (rec2.height/2)
-    };
-
-    return sr_vec2_normalize(sr_vector2_sub(rec2_center, rec1_center));
-}
-
-//Takes two rects and resolve the position of the second one
-static sr_rec sr_resolver_rects_collision(sr_rec static_rec, sr_rec rec_to_move) {
-	if (!sr_check_collision_recs(static_rec, rec_to_move)) {
-		return rec_to_move;
+		return true;
 	} else {
-		sr_rec collision_rec = sr_get_collision_rec(static_rec, rec_to_move);
-		sr_vec2 center_distance_vec = sr_get_center_distance_vec(static_rec, rec_to_move);
-		sr_rec rec_copy = rec_to_move;
-
-		if (fabs(center_distance_vec.x) > fabs(center_distance_vec.y)) {
-            if (center_distance_vec.x > 0) {
-                rec_copy.x = rec_copy.x + collision_rec.width;
-            } else {
-                rec_copy.x = rec_copy.x - collision_rec.width;
-            }
-        } else {
-            if (center_distance_vec.y > 0) {
-                rec_copy.y = rec_copy.y + collision_rec.height;
-            } else {
-                rec_copy.y = rec_copy.y - collision_rec.height;
-            }
-        }
-
-		return rec_copy;
-	}
+        return false;
+    }
 }
 
-// Move and collide rect with rects
-static sr_rec sr_move_and_collide(sr_rec *static_recs, int number_of_recs, sr_rec rec_to_move, sr_vec2 velocity) {
-    sr_rec before_move = rec_to_move;
+// Check collision between a ray and a rectangle
+static bool sr_check_ray_vs_rec_collision(const sr_ray2 ray, const sr_rec target, sr_vec2 *contact_point, sr_vec2 *contact_normal, float *t_hit_near) {
+    // Cache division
+    sr_vec2 indiv = sr_vec2_divide((sr_vec2){1.0f, 1.0f}, ray.direction);
 
-    rec_to_move.x += velocity.x;
-    rec_to_move.y += velocity.y;
+    // Calculate intersections with rectangle bounding axes
+    sr_vec2 t_near = sr_vec2_multiply(
+        sr_vector2_sub((sr_vec2){ target.x, target.y }, ray.position),
+        indiv
+    );
+    sr_vec2 t_far = sr_vec2_multiply(
+        sr_vector2_sub(
+            sr_vector2_add((sr_vec2){target.x, target.y}, (sr_vec2){target.width, target.height}),
+            ray.position
+        ),
+        indiv
+    );
 
-    sr_rec result = rec_to_move;
+    // Check for nan
+    if (isnanf(t_far.y) || isnanf(t_far.x)) return false;
+	if (isnanf(t_near.y) || isnanf(t_near.x)) return false;
 
-    for (int i = 0; i < number_of_recs; i++) {
-        result = sr_resolver_rects_collision(static_recs[i], result);
-    }
+    // Sort distances
+    if (t_near.x > t_far.x) swap_float(&t_near.x, &t_far.x);
+    if (t_near.y > t_far.y) swap_float(&t_near.y, &t_far.y);
 
-    if (result.x == before_move.x && result.y == before_move.y) {
-        result.x += velocity.x;
-        result.y += velocity.y;
-        for (int i = (number_of_recs-1); i > -1; i--) {
-            result = sr_resolver_rects_collision(static_recs[i], result);
-        }
-    }
+    // Early rejection		
+	if (t_near.x > t_far.y || t_near.y > t_far.x) return false;
 
-    return result;
+    // Closest 'time' will be the first contact
+    *t_hit_near = max(t_near.x, t_near.y);
+
+    // Furthest 'time' is contact on opposite side of target
+    float t_hit_far = min(t_far.x, t_far.y);
+
+    // Reject if ray direction is pointing away from object
+    if (t_hit_far < 0) return false;
+
+    // Contact point of collision from parametric line equation
+    *contact_point = sr_vector2_add(ray.position, sr_vec2_scale(ray.direction, *t_hit_near));
+
+	if (t_near.x > t_near.y)
+		if (ray.direction.x < 0)
+			*contact_normal = (sr_vec2){ 1, 0 };
+		else
+			*contact_normal = (sr_vec2){ -1, 0 };
+	else if (t_near.x < t_near.y)
+		if (ray.direction.y < 0)
+			*contact_normal = (sr_vec2){ 0, 1 };
+		else
+			*contact_normal = (sr_vec2){ 0, -1 };
+
+    return true;
 }
 
+static bool sr_dynamic_rect_vs_rect(const sr_rec in, const sr_rec target, sr_vec2 vel, sr_vec2 *contact_point, sr_vec2 *contact_normal, float *contact_time, float delta) {
+    // Check if dynamic rectangle is actually moving - we assume rectangles are NOT in collision to start
+    if (vel.x == 0 && vel.y == 0) return false;
+    
+	// Expand target rectangle by source dimensions
+    sr_rec expanded_target;
+    expanded_target.x = target.x - (in.width/2);
+    expanded_target.y = target.y - (in.height/2);
+    expanded_target.width = target.width + in.width;
+    expanded_target.height = target.height + in.height;
+
+    if (sr_check_ray_vs_rec_collision(
+      (sr_ray2){
+        (sr_vec2){
+          in.x + (in.width/2),
+          in.y + (in.height/2)
+        },
+        sr_vec2_scale(vel, delta)
+      },
+      expanded_target,
+      contact_point,
+      contact_normal,
+      contact_time
+    )) {
+      if (*contact_time <= 1.0f && *contact_time >= -1.0f) return true;
+    }
+
+    return false;
+}
+
+void sr_sort_indexes(sr_sort_pair *times, int length) {
+    sr_sort_pair key;
+    int i, j;
+
+    for (i = 1; i < length; i++) {
+      key = times[i];
+      j = i - 1;
+
+      while(j >= 0 && times[j].time > key.time) {
+        times[j+1] = times[j];
+        j = j -1;
+      }
+
+      times[j + 1] = key;
+
+    }
+
+    // for (int i = 0; i < length; ++i) {
+    //   for (int j = i + 1; j < length; ++j) {
+    //     if (times[i].time > times[j].time) {
+    //       key =  times[i];
+    //       times[i] = times[j];
+    //       times[j] = key;
+		// 		}
+		// 	}
+    // }
+}
+
+static void sr_move_and_slide(sr_rec *obstacles, int obstacles_length, sr_vec2 hitbox, sr_vec2 *vel, sr_vec2 *pos , float delta) {
+	sr_sort_pair times[obstacles_length];
+	sr_vec2 cp, cn;
+	float time = 0;
+
+  	sr_rec hitbox_rec = {
+  	  	pos->x - (hitbox.x/2),
+  	  	pos->y - (hitbox.y/2),
+  	  	hitbox.x,
+  	  	hitbox.y
+  	};
+
+
+	for (int i = 0; i < obstacles_length; i++) {
+    	sr_dynamic_rect_vs_rect(hitbox_rec, obstacles[i], *vel, &cp, &cn, &time, delta);
+    	times[i].index = i;
+    	times[i].time = time;
+  	}
+
+	sr_sort_indexes(times, obstacles_length);
+
+	for (int i = 0; i < obstacles_length; i++) {  
+    	if (sr_dynamic_rect_vs_rect(hitbox_rec, obstacles[times[i].index], *vel, &cp, &cn, &time, delta)) {
+    	    pos->x = cp.x;
+    	    pos->y = cp.y;
+
+			if (fabs(cn.x)) {
+    	    	vel->x = 0;
+    	    }
+
+    	    if (fabs(cn.y)) {
+    	    	vel->y = 0;
+    	    }
+    	}
+  	}
+}
 #endif
